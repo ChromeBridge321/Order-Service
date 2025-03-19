039874f3274e5c33e7ae3c0f8c438270a97529b5
ChromeBridge321 <davidgarcia2809@gmail.com>
2025-03-19 10:34:01 -0600

    .

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,3 @@ Route::prefix('v1')->middleware(['api','jwt.verify'])->group(function(){
         Route::delete('/{id}', [OrderController::class,'destroy']);
     });
 });
-
-    Route::prefix(('orders'))->group(function(){
-        Route::get('/return',[OrderController::class,'return']);
-    });   
-
